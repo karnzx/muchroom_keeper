68d79b79e532dee8592ec3773c2a5522bee6fcce
piravit <karnzx-18762@hotmail.com>
2019-02-27 18:20:50 +0700

    update shop

--- APPMAIN/src/main/java/pl/pawelkleczkowski/mushroom_keeper/cart.java
+++ APPMAIN/src/main/java/pl/pawelkleczkowski/mushroom_keeper/cart.java
@@ -13,12 +13,15 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -32,7 +35,12 @@ import okhttp3.Response;
 public class cart extends AppCompatActivity {
     private static final String MY_PREFS = "my_prefs";
     private ArrayList<String> amount = new ArrayList<>();
+    private ArrayList<String> ArrInfoName = new ArrayList<>();
+    private ArrayList<Integer> ArrInfoPic = new ArrayList<>();
+    private ArrayList<String> ArrInfoDescrib = new ArrayList<>();
+    private ArrayList<Integer> ArrInfoPrice = new ArrayList<>();
     private int Position;
+    private int IndexName;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -46,6 +54,10 @@ public class cart extends AppCompatActivity {
         Button button = findViewById(R.id.btt);
         TextView IDline = findViewById(R.id.idline);
         TextView username = findViewById(R.id.name);
+        ImageView infoPic = findViewById(R.id.infoPic);
+        TextView infoName = findViewById(R.id.infoName);
+        TextView infoDescrib = findViewById(R.id.infoDescrib);
+        TextView infoPrice = findViewById(R.id.infoPrice);
 
         SharedPreferences shared = getSharedPreferences(MY_PREFS, Context.MODE_PRIVATE); //point to my_prefs
         IDline.setText(getString(R.string.IDline));
@@ -57,12 +69,20 @@ public class cart extends AppCompatActivity {
 
         //Spinner
         Spinner Amount1 = findViewById(R.id.Amount1);
+        Spinner Select = findViewById(R.id.select);
         setValueAmount();
+        setValueName();
+        setValuePic();
+        setValueDescrib();
+        setValuePrice();
 
         // Adapter
         ArrayAdapter<String> adapterAmount = new ArrayAdapter<>(this,
                 android.R.layout.simple_dropdown_item_1line, amount);
+        ArrayAdapter<String> adapterSelect = new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line, ArrInfoName);
         Amount1.setAdapter(adapterAmount);
+        Select.setAdapter(adapterSelect);
 
         Amount1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -75,17 +95,32 @@ public class cart extends AppCompatActivity {
             }
         });
 
+        Select.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                IndexName = position;
+                infoName.setText(ArrInfoName.get(position));
+                infoPic.setImageResource(ArrInfoPic.get(position));
+                infoDescrib.setText(ArrInfoDescrib.get(position));
+                infoPrice.setText(String.format(Locale.getDefault(),"%d",ArrInfoPrice.get(position)));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         buy_btt1.setOnClickListener((View v) -> {
             String IDLINE = shared.getString("IDLINE","");
             String USERNAME = shared.getString("USERNAME","");
-            if(Position != 0){
+            if(Position != 0 && IndexName != 0){
                 if(!IDLINE.equals("") && !USERNAME.equals("")) {
                     run(setMessage(IDLINE,USERNAME));
                 }else{
                     Toast.makeText(cart.this,"กรุณาตั้งชื่อก่อน",Toast.LENGTH_SHORT).show();      //please set your INFORMATION first
                 }
             }else{
-                Toast.makeText(cart.this,"โปรดเลือกจำนวน",Toast.LENGTH_SHORT).show();   //please select amount
+                Toast.makeText(cart.this,"โปรดเลือกจำนวน หรือ ชนิดของเห็ด",Toast.LENGTH_SHORT).show();   //please select amount
             }
         });
 
@@ -94,7 +129,7 @@ public class cart extends AppCompatActivity {
 
     private void setValueAmount() {
 
-        amount.add("จำนวน");
+        amount.add("ชิ้น");
         amount.add("1 ชิ้น");
         amount.add("2 ชิ้น");
         amount.add("3 ชิ้น");
@@ -102,13 +137,44 @@ public class cart extends AppCompatActivity {
         amount.add("5 ชิ้น");
 
     }
+    private void setValueName() {
+
+        ArrInfoName.add(getString(R.string.please_select_kind));
+        ArrInfoName.add(getString(R.string.mr_sell1));
+        ArrInfoName.add(getString(R.string.mr_sell2));
+
+    }
+    private void setValuePic() {
+
+        ArrInfoPic.add(R.drawable.mr3);
+        ArrInfoPic.add(R.drawable.forsell);
+        ArrInfoPic.add(R.drawable.forsell2);
+
+    }
+    private void setValueDescrib() {
+
+        ArrInfoDescrib.add("");
+        ArrInfoDescrib.add(getString(R.string.info_sell1));
+        ArrInfoDescrib.add(getString(R.string.info_sell2));
+
+    }
+    private void setValuePrice() {
+
+        ArrInfoPrice.add(0);
+        ArrInfoPrice.add(120);
+        ArrInfoPrice.add(190);
+
+    }
 
     private String setMessage(String IDLINE,String USERNAME){
+        int price = (ArrInfoPrice.get(IndexName)*Position);
         String msg = "\n";  //break line first for space!
         msg += getString(R.string.IDline) + " : " + IDLINE + "\n";
         msg += getString(R.string.username) + " : " + USERNAME + "\n";
-        msg += "ORDER : " + getString(R.string.mr_sell2) + "\n";
-        msg += "จำนวน : " + amount.get(Position);
+        msg += "order : " + ArrInfoName.get(IndexName) + "\n";
+        msg += "ชิ้นละ : " + ArrInfoPrice.get(IndexName) + "\n";
+        msg += "จำนวน : " + amount.get(Position) + "\n";
+        msg += "เป็นเงิน : " + String.format(Locale.getDefault(),"%d",price) + "บาท";
         return msg;
     }
     private void run(String message) {
